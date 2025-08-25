0ac9eaf508b691d870a447c762a5f73aacf0b327
Pheonix <arshi900389@gmail.com>
2025-08-25 13:39:41 +0530

    Bubble Sort

---
+++ Sorting/Bubble.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Main{
+    public static int[] bubbleSort(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+        }
+        return nums;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+        System.out.print("Enter elements: ");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        nums = bubbleSort(nums);
+
+        System.out.print("Sorted array: ");
+        System.out.println(Arrays.toString(nums));
+    }
+}
